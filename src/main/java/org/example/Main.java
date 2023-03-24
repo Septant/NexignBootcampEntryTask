d80801ae21de4af62b1b1a012cea2a5201060e84
Septant <orcams@yandex.ru>
2023-03-24 16:10:51 +0300

    16:10 24.03.23 test-build(upd)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,53 +28,55 @@ public class Main {
         }
 
         String[] incomingData = incomingDataList.toArray(new String[0]);
+        while (true) {
+            CDRs cdrs = new CDRs(incomingData);
 
-        CDRs cdrs = new CDRs(incomingData);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the phone number to generate a report\n");
+            long number = parseLong(scanner.nextLine());
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the phone number to generate a report\n");
-        long number = parseLong(scanner.nextLine());
+            boolean flag = false;
 
-        boolean flag = false;
-
-        for (int i = 0; i < incomingData.length; i++) {
-            if (cdrs.cdrList.get(i).getAgentNumber() == number) {
-                flag = true;
-                break;
-            }
-        }
-        if (flag) {
-            String report = "D:\\practice\\NexignBootcampEntry\\NexignBootcampEntryTask\\reports\\report.txt";
-            CDRs filteredByNumber = cdrs.FilterByNumber(cdrs.cdrList, number);
-            try {
-                FileWriter writer = new FileWriter(report);
-                writer.write("Tariff Index: "+filteredByNumber.cdrList.get(0).getTariff().getTariffType()+"\n");
-                writer.write("----------------------------------------------------------------------------\n");
-                writer.write("Report for phone number " + filteredByNumber.cdrList.get(0).getAgentNumber()+"\n");
-                writer.write("----------------------------------------------------------------------------\n");
-                writer.write("| Call Type |     Start Time      |       End Time      | Duration | Cost |\n");
-                writer.write("----------------------------------------------------------------------------\n");
-                for (CDR cdr: filteredByNumber.cdrList) {
-                    writer.write("|     "+cdr.getCallType()+"     | "+cdr.dateFormat(cdr.getCallStart())+
-                            " | "+cdr.dateFormat(cdr.getCallEnd())+" | "+cdr.dateFormat(cdr.getCallDuration())+" |  " +
-                            ""+cdrs.getCallCost(cdr)+" |\n");
+            for (int i = 0; i < incomingData.length; i++) {
+                if (cdrs.cdrList.get(i).getAgentNumber() == number) {
+                    flag = true;
+                    break;
                 }
-                writer.write("----------------------------------------------------------------------------\n");
-                writer.write("|                                           Total Cost: |" +
-                        "    "+filteredByNumber.agentBilling(filteredByNumber.cdrList) +" rubles   |\n");
-                writer.write("----------------------------------------------------------------------------");
-
-                writer.close();
-                System.out.println("Successful!");
-            } catch (IOException e) {
-                System.out.println("An error occurred while writing to file " + report);
-                e.printStackTrace();
             }
-        } else {
-            System.out.print("Number not found");
+            if (flag) {
+                String report = "D:\\practice\\NexignBootcampEntry\\NexignBootcampEntryTask\\reports\\report.txt";
+                CDRs filteredByNumber = cdrs.FilterByNumber(cdrs.cdrList, number);
+                try {
+                    FileWriter writer = new FileWriter(report);
+                    writer.write("Tariff Index: " + filteredByNumber.cdrList.get(0).getTariff().getTariffType() + "\n");
+                    writer.write("----------------------------------------------------------------------------\n");
+                    writer.write("Report for phone number " + filteredByNumber.cdrList.get(0).getAgentNumber() + "\n");
+                    writer.write("----------------------------------------------------------------------------\n");
+                    writer.write("| Call Type |     Start Time      |       End Time      | Duration | Cost |\n");
+                    writer.write("----------------------------------------------------------------------------\n");
+                    for (CDR cdr : filteredByNumber.cdrList) {
+                        writer.write("|     " + cdr.getCallType() + "     | " + cdr.dateFormat(cdr.getCallStart()) +
+                                " | " + cdr.dateFormat(cdr.getCallEnd()) + " | " + cdr.dateFormat(cdr.getCallDuration()) + " |  " +
+                                "" + cdrs.getCallCost(cdr) + " |\n");
+                    }
+                    writer.write("----------------------------------------------------------------------------\n");
+                    writer.write("|                                           Total Cost: |" +
+                            "    " + filteredByNumber.agentBilling(filteredByNumber.cdrList) + " rubles   |\n");
+                    writer.write("----------------------------------------------------------------------------");
+
+                    writer.close();
+                    System.out.println("Successful!");
+                } catch (IOException e) {
+                    System.out.println("An error occurred while writing to file " + report);
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.print("Number not found");
+            }
+
+            // ввод номера
+            System.out.println("Hello world!");
         }
 
-        // ввод номера
-        System.out.println("Hello world!");
     }
 }
